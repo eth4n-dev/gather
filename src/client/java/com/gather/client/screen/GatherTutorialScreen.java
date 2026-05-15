a72ea1d235290dde61c79aa5ae4c53e13fb44073
eth4n-dev <ethanpierini7@gmail.com>
2026-05-15 12:49:59 +0200

    Tighten trade tutorial placement

--- src/client/java/com/gather/client/screen/GatherTutorialScreen.java
+++ src/client/java/com/gather/client/screen/GatherTutorialScreen.java
@@ -250,8 +250,7 @@ public class GatherTutorialScreen extends Screen {
 
     private int targetCardX(int cw, Step s, int[] hl) {
         if (s.bg() == BgType.TRADE) {
-            int merchantX = width / 2 - 88;
-            int x = merchantX + 176 + 8;
+            int x = hl != null ? hl[0] + hl[2] + 8 : width / 2 + 88;
             return Math.max(8, Math.min(width - cw - 8, x));
         }
         if (s.pos() == CardPos.CENTER) {
@@ -549,7 +548,7 @@ public class GatherTutorialScreen extends Screen {
         drawTradeRow(ctx, panelX + 5, panelY + 76, false,
                 Items.EMERALD.getDefaultStack(), Items.ROTTEN_FLESH.getDefaultStack(), Items.GOLDEN_APPLE.getDefaultStack(),
                 "Golden Apple");
-        int detailTop = panelY + 100;
+        int detailTop = panelY + 110;
         ctx.drawTextWithShadow(textRenderer, Text.literal("Want"), panelX + 6, detailTop + 4, 0xFF8899AA);
         drawTradeAmountField(ctx, panelX + 45, detailTop + 1);
         ctx.drawTextWithShadow(textRenderer, Text.literal("64"), panelX + 104, detailTop + 4, 0xFFCCDDFF);
